ad753e9682287eb5189beffaf39d8bb4f2cc1221
Adebayoisreal122 <adebayoisrealight122@gmail.com>
2026-05-13 11:02:17 +0100

    I fixed the global CSS error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-// import './globals.css'
+import './globals.css'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from '@/context/AuthContext'
 import { CartProvider } from '@/context/CartContext'
